a0e408eb29fe8c67b4658b6c12a47631ce009b81
bukola.thompson <bukola.thompson@outlook.com>
2020-09-11 13:47:04 +0100

    inital commit

---
+++ db.js
@@ -0,0 +1,34 @@
+{
+  "thermostats": [
+    {
+        "thermostatId": 1,
+        "imageUrl": "https://i.pinimg.com/564x/56/f7/0a/56f70aca3170062c6f5e22d834c3f3fd.jpg",
+        "name": "nigeria",
+        "deadline": "12/09/2021",
+        "creationDate": "12/09/2020",
+        "capitalGoal": 10,
+        "currentCapital": 50,
+        "remainingCapital": 40
+    },
+    {
+        "thermostatId": 2,
+        "imageUrl": "https://i.pinimg.com/564x/8e/74/f3/8e74f3b2ae463bd38b356a81c089b44c.jpg",
+        "name": "teeth whitening",
+        "deadline": "12/20/2020",
+        "creationDate": "12/09/2020",
+        "capitalGoal": 500,
+        "currentCapital": 35,
+        "remainingCapital": 0
+    },
+    {
+        "thermostatId": 2,
+        "imageUrl": "https://i.pinimg.com/564x/34/11/c1/3411c1149a31de5d2c7d45547dabf35f.jpg",
+        "name": "YSL Bag",
+        "deadline": "12/12/2020",
+        "creationDate": "12/09/2020",
+        "capitalGoal": 800,
+        "currentCapital": 70,
+        "remainingCapital": 0
+    }
+  ]
+}
